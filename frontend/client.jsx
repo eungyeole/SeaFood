1b4830d7d7995a5c35203c0259881470c6332e30
eungyeole <66717787+eungyeole@users.noreply.github.com>
2020-08-18 15:38:28 +0900

    [Update] SeaFood

--- frontend/client.jsx
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const { hot } = require('react-hot-loader/root');
-
-const App = require('./src/components/Routes');
-const Hot = hot(App);
-
-ReactDOM.render(<Hot />, document.querySelector('#root'));
