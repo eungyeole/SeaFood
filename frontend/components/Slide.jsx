3d0ac14a4ed9acf3ba98717dc24d87075ddfb711
eungyeole <66717787+eungyeole@users.noreply.github.com>
2020-08-18 17:20:13 +0900

    [MODIFY] 슬라이드 구현

--- frontend/components/Slide.jsx
+++ frontend/components/Slide.jsx
@@ -2,12 +2,42 @@ const React = require('react');
 const { useState, useRef, memo } = React;
 require('../app.scss');
 
-const Slide = memo( () => {
 
+const Slide = memo( () => {
+    Next=()=>{
+        let last=document.getElementsByClassName("slide").length;
+        for(let i=0;i<last;i++)
+        {
+            if(document.getElementsByClassName("slide")[i].style.display=='block'){
+                document.getElementsByClassName("slide")[i].style.display='none';
+                if(i+1>last-1)
+                {
+                    document.getElementsByClassName("slide")[0].style.display='block';
+                }
+                else{
+                    document.getElementsByClassName("slide")[i+1].style.display='block';
+                }
+                break;
+            }
+        }
+    }
     return (
         <>
-            <div className="slide">
-                
+            <div className="slide-wrapper">
+                <div style={{display:'block'}} className="slide">
+                    <div className="yellow"></div>
+                </div>
+                <div className="slide">
+                    <div className="blue"></div>
+                </div>
+                <div className="slide">
+                    <div className="red"></div>
+                </div>
+                <div className="slide">
+                    <div className="orange"></div>
+                </div>
+                <div className="slide"></div>
+                <button className="next" onClick={Next}></button>
             </div>
         </>
     );
