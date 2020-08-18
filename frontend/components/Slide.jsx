69d77ad78520a342f006ac3b6e15e5ba53f98f60
eungyeole <66717787+eungyeole@users.noreply.github.com>
2020-08-18 18:50:36 +0900

    [ADD] 슬라이드 Prev버튼 추가

--- frontend/components/Slide.jsx
+++ frontend/components/Slide.jsx
@@ -19,6 +19,23 @@ const Slide = memo( () => {
             }
         }
     }
+    Prev=()=>{
+        let last=document.getElementsByClassName("slide").length;
+        for(let i=0;i<last;i++)
+        {
+            if(document.getElementsByClassName("slide")[i].style.display=='block'){
+                document.getElementsByClassName("slide")[i].style.display='none';
+                if(i-1==-1)
+                {
+                    document.getElementsByClassName("slide")[last-1].style.display='block';
+                }
+                else{
+                    document.getElementsByClassName("slide")[i-1].style.display='block';
+                }
+                break;
+            }
+        }
+    }
     const Timer=setInterval(()=>{
         Next();
     },10000);
@@ -38,6 +55,7 @@ const Slide = memo( () => {
                     <div className="orange"></div>
                 </div>
                 <div className="slide"></div>
+                <button className="prev" onClick={Prev}></button>
                 <button className="next" onClick={Next}></button>
             </div>
         </>
