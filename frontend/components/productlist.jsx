2b991137e08db0698a1a02dde3774cd988efbbf3
eungyeole <66717787+eungyeole@users.noreply.github.com>
2020-08-20 17:04:34 +0900

    [CREATE] 상품디자인 제작

--- frontend/components/productlist.jsx
+++ frontend/components/productlist.jsx
@@ -24,7 +24,75 @@ const Productlist = memo( () => {
                         <div className="product-details"><i className="fas fa-caret-down"></i></div>
                     </div>
                     <ul className="product-list">
-                        <li></li>
+                        <li>
+                            <div className="image-box">
+                                <img src="https://us.123rf.com/450wm/bestfotostudio/bestfotostudio1509/bestfotostudio150900150/45464411-타이거-새우-흰색-배경에-격리-된-새우-해물.jpg?ver=6" alt=""/>
+                            </div>
+                            <div className="information-box">
+                                <div className="price-box">
+                                    <span className="product-price">54,900</span>
+                                    <span className="product-unit">원</span>
+                                </div>
+                                <div className="product-name">45464411-타이거-새우</div>
+                                <span className="product-tag">구매 694</span>
+                            </div>
+                            <div className="button-box">
+                                <div className="product-button"><i class="far fa-credit-card"></i> 구매하기</div>
+                                <div className="product-button cok"><i class="fas fa-shopping-cart"></i></div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="image-box">
+                                <img src="https://us.123rf.com/450wm/bestfotostudio/bestfotostudio1509/bestfotostudio150900150/45464411-타이거-새우-흰색-배경에-격리-된-새우-해물.jpg?ver=6" alt=""/>
+                            </div>
+                            <div className="information-box">
+                                <div className="price-box">
+                                    <span className="product-price">54,900</span>
+                                    <span className="product-unit">원</span>
+                                </div>
+                                <div className="product-name">45464411-타이거-새우</div>
+                                <span className="product-tag">구매 694</span>
+                            </div>
+                            <div className="button-box">
+                                <div className="product-button"><i class="far fa-credit-card"></i> 구매하기</div>
+                                <div className="product-button cok"><i class="fas fa-shopping-cart"></i></div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="image-box">
+                                <img src="https://us.123rf.com/450wm/bestfotostudio/bestfotostudio1509/bestfotostudio150900150/45464411-타이거-새우-흰색-배경에-격리-된-새우-해물.jpg?ver=6" alt=""/>
+                            </div>
+                            <div className="information-box">
+                                <div className="price-box">
+                                    <span className="product-price">54,900</span>
+                                    <span className="product-unit">원</span>
+                                </div>
+                                <div className="product-name">45464411-타이거-새우</div>
+                                <span className="product-tag">구매 694</span>
+                            </div>
+                            <div className="button-box">
+                                <div className="product-button"><i class="far fa-credit-card"></i> 구매하기</div>
+                                <div className="product-button cok"><i class="fas fa-shopping-cart"></i></div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="image-box">
+                                <img src="https://us.123rf.com/450wm/bestfotostudio/bestfotostudio1509/bestfotostudio150900150/45464411-타이거-새우-흰색-배경에-격리-된-새우-해물.jpg?ver=6" alt=""/>
+                            </div>
+                            <div className="information-box">
+                                <div className="price-box">
+                                    <span className="product-price">54,900</span>
+                                    <span className="product-unit">원</span>
+                                </div>
+                                <div className="product-name">45464411-타이거-새우</div>
+                                <span className="product-tag">구매 694</span>
+                            </div>
+                            <div className="button-box">
+                                <div className="product-button"><i class="far fa-credit-card"></i> 구매하기</div>
+                                <div className="product-button cok"><i class="fas fa-shopping-cart"></i></div>
+                            </div>
+                        </li>
+
                     </ul>
                 </div>
             </div>
